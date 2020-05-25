051fb7a6403912222326a95072fa0fa7846c2cda
naysok_win10 <ysok.ysok@gmail.com>
2020-05-26 01:11:35 +0900

    [update] __init__.py

---
+++ prepare_operate_geometry/__init__.py
@@ -0,0 +1,16 @@
+###
+
+import Rhino
+import Rhino.Geometry as rg
+import rhinoscriptsyntax as rs
+import scriptcontext as sc
+
+
+###
+
+from bake_geometry import *
+from create_geometry import *
+from rs_command import *
+from util import *
+
+###
