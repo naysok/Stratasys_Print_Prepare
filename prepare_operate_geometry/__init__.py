13e8475aa1e4c52d9431c5487284f6b02d7438ab
naysok_win10 <ysok.ysok@gmail.com>
2020-05-26 23:40:17 +0900

    [update] slice

--- prepare_operate_geometry/__init__.py
+++ prepare_operate_geometry/__init__.py
@@ -1,4 +1,13 @@
+####################################
+###                              ###
+###     Modules for ghPython     ###
+###     IronPython 2.7.8         ###
+###                              ###
+####################################
+
+
 ###
+
 
 import Rhino
 import Rhino.Geometry as rg
@@ -8,9 +17,12 @@ import scriptcontext as sc
 
 ###
 
+
 from bake_geometry import *
 from create_geometry import *
 from rs_command import *
+from slice_geometry import *
 from util import *
+
 
 ###
