0e322e2403c154909f1a9fc900b7f8a93c43b5ab
naysok_win10 <ysok.ysok@gmail.com>
2020-08-16 01:08:24 +0900

    [update] trim, resize

--- prepare_image_processing/image_processing.py
+++ prepare_image_processing/image_processing.py
@@ -21,9 +21,30 @@ class ImageProcessing():
         return gray
 
 
-    def draw_crosshair(self, pos_x, pos_y):
-        pass
+    def draw_crosshair(self, img, pt):
+        draw = ImageDraw.Draw(img)
+        posx, posy = pt
+        draw.line((posx, 0, posx, img.height), fill=(0, 255, 0))
+        draw.line((0, posy, img.width, posy), fill=(0, 0, 255))
+        return img
 
 
-    def scale_by_target_size(self):
-        pass
+    def draw_x_line(self, img, pt0, pt1):
+        draw = ImageDraw.Draw(img)
+        posx0, posy0 = pt0
+        posx1, posy1 = pt1
+        draw.line((posx0, posy0, posx1, posy1), fill=(0, 255, 0))
+        draw.line((posx0, posy1, posx1, posy0), fill=(0, 0, 255))
+        return img
+
+
+    def trim_w_offset(self, paste_img, canvas_size, off):
+        new = Image.new("RGBA", (canvas_size, canvas_size), (0, 0, 0, 0))
+        off_x, off_y = off
+        new.paste(paste_img, (off_x * (-1), off_y * (-1)))
+        return new
+
+
+    def change_scale(self, img, target_size):
+        img_resize = img.resize((target_size, target_size), Image.LANCZOS)
+        return img_resize
